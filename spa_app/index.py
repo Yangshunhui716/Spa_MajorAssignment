13af354b251e159fe5b1075c010be9de23577713
Yangshunhui716 <thieuhuy856@gmail.com>
2025-12-10 22:54:39 +0700

    File code

--- spa_app/index.py
+++ spa_app/index.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("appointments.html")
 
+@app.route('/1')
+def index1():
+    return render_template("servicessheet.html")
+
+@app.route('/2')
+def index2():
+    return render_template("payment.html")
+
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True)
